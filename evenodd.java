48211c1564fa1889f5a8287d408f723c779c8e55
rohini1996 <rohini111222@gmail.com>
2017-06-15 12:08:40 +0530

    evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+class eo
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("even number");
+        }
+        else
+        {
+            System.out.println("odd number");
+        }
+    }
+}
